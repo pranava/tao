aad4c26a9454138f86c0704610f3c89836b7b8c4
Pranava Adduri <abhinadduri@gmail.com>
2014-11-30 23:22:46 -0800

    Added scheduler and UI changes.
    - Fixed position storing while opening a file
    - Added a simulation description field to the globals panel
    - New scheduler.terminate() function added to stop simulation on call
    - Trace events working
    - Multiple edges from node x to node y now considered

--- engine/nxt.js
+++ engine/nxt.js
@@ -1,11 +1,4 @@
-// time for each event + parent
-//event unique id's
-
-// the times need double checking because they sometimes dont print accurate timestamps
-// i think the problem is because you wanted to do it through the scheduler, but the order in which events are scheduled doesn't always equate to the 
-// order in which they are executed. can we talk about this tomorrow?
-
-function Event(name, parent, id, count, timestamp, priority, func, params) {
+function Event(name, parent, id, count, timestamp, priority, func, params, trace) {
   this.parent = parent;
   this.name = name;
   this._id = id;
@@ -14,6 +7,7 @@ function Event(name, parent, id, count, timestamp, priority, func, params) {
   this.priority = priority;
   this.func = func;
   this.params = params;
+  this.trace = trace;
 }
 
 function lifoRank(e1, e2) {
@@ -33,12 +27,11 @@ function lifoRank(e1, e2) {
 }
 
 
-
-
 function Scheduler(eventRanker) {
   var self = this;
   var _clock = 0;
   var _count = 0;
+  var _terminate = false;
 
   var _uniqueID = {};
 
@@ -52,7 +45,11 @@ function Scheduler(eventRanker) {
     return _count;
   }
 
-  self.schedule = function(name, parent, offset, priority, func, params) {
+  self.terminate = function() {
+    _terminate = true;
+  }
+
+  self.schedule = function(name, parent, offset, priority, func, params, trace) {
     
     if (!_uniqueID.hasOwnProperty(name)) {
       _uniqueID[name] = 0;
@@ -60,13 +57,17 @@ function Scheduler(eventRanker) {
       _uniqueID[name] += 1;
     }
 
-    var futureEvent = new Event(name, parent, _uniqueID[name], _count, _clock + offset, priority, func, params);
+    var futureEvent = new Event(name, parent, _uniqueID[name], _count, _clock + offset, priority, func, params, trace);
     _pendingEvents.push(futureEvent);
     _count += 1;
   }
 
   self.hasNext = function() {
-    return !_pendingEvents.empty();
+    if (!_terminate) {
+      return !_pendingEvents.empty();
+    } else {
+      return false;
+    }
   }
 
   self.next = function() {
@@ -101,7 +102,7 @@ function Engine(eventRanker) {
 
       if (currentEvent.parent.name == "Run") {
         console.log(currentEvent.name + ' ' + currentEvent._id + ' by Run 0');
-      } else if (currentEvent.parent) {
+      } else if (currentEvent.parent && currentEvent.trace) {
         console.log(currentEvent.name + ' ' + currentEvent._id + ' by ' + currentEvent.parent.name
          + ' ' + currentEvent.parent._id + ' at time '
         + scheduler.getClock());
